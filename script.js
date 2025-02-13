66e0c52c0a044092d6efb1257ce4972591930b6d
hauspaws <amrys.wang@gmail.com>
2025-02-14 02:46:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.querySelector(".menu-button");
     const menu = document.getElementById("menu");
+    const closeButton = document.querySelector(".close-button");
 
-    menuButton.addEventListener("click", function () {
+    function toggleMenu() {
         menu.classList.toggle("show-menu");
-    });
+    }
 
+    // Toggle menu on button click
+    menuButton.addEventListener("click", toggleMenu);
+    closeButton.addEventListener("click", toggleMenu);
+
+    // Close menu when clicking outside
     document.addEventListener("click", function (event) {
         if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
             menu.classList.remove("show-menu");
